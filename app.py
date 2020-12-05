24d4507178eeff5e821493bec1129e8cb3b1cb2b
ravi0497 <muhammadravi04@gmail.com>
2020-12-06 00:50:45 +0700

    Initial Commit

---
+++ app.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 6 00:47:45 2020
+
+@author: Muhammad Ravi
+"""
+
+import requests
+import pandas as pd
+import regex as re
+from bs4 import BeautifulSoup
+
